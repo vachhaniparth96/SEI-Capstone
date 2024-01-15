fcbb38ea3a31049e9c33ee6686dc3e1c5c0e0111
Parth Vachhani <vachhani.parth.96@gmail.com>
2024-01-15 13:42:00 -0500

    Implement product detail route

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -3,14 +3,14 @@ const Product = require('../models/Product');
 module.exports = {
     createProduct,
     getProducts,
+    productDetails,
 }
 
+//Create products
 async function createProduct(req,res) {
     const product = await Product.create(req.body);
     try {
-        res.status(201).json({
-            product,
-        });
+        res.status(201).json({product});
     } catch(err) {
         res.status(409).json({
             message: err.message,
@@ -18,11 +18,25 @@ async function createProduct(req,res) {
     }
 }
 
+//Get all products
 async function getProducts(req, res) {
     try {
         const products = await Product.find();
         res.status(200).json(products);
     } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+//Get single product
+async function productDetails(req, res) {
+    try{
+        const product = await Product.findById(req.params.id);
+        if(!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json(product);
+    }catch(err){
         res.status(500).json({ message: err.message });
     }
 }
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const productsCtrl = require('../controllers/products');
 
+//GET- all products
 router.get('/', productsCtrl.getProducts);
+
+//GET- a single product
+router.get('/:id', productsCtrl.productDetails);
+
+//POST- create a product
 router.post('/admin/products', productsCtrl.createProduct);
 
 module.exports = router;

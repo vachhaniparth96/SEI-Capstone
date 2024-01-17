4dd309d2ad0015c2db0632731e1ca5dfb5eb4720
Parth Vachhani <vachhani.parth.96@gmail.com>
2024-01-17 15:38:56 -0500

    Implement functionality to let user update their profile and admin functionality to view all users as well as update and delete user profiles

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,11 @@ const User = require('../models/User')
 
 module.exports = {
     getUsers,
+    updateProfile,
+    getAllUsers,
+    userDetails,
+    updateUsers,
+    deleteUsers,
 }
 
 async function getUsers(req,res,next) {
@@ -9,5 +14,74 @@ async function getUsers(req,res,next) {
 
     res.status(200).json({
         user,
+    })
+}
+
+async function updateProfile(req,res,next) {
+    const newData = {
+        name: req.body.name,
+        email: req.body.email,
+
+    }
+
+    const user = await User.findByIdAndUpdate(req.user._id, newData, { new: true })
+
+    res.status(200).json({
+        user,
+    });
+    
+}
+
+async function getAllUsers(req,res,next) {
+    const users = await User.find();
+
+    res.status(200).json({
+        users,
+    })
+}
+
+async function userDetails(req,res,next) {
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+        return res.status(404).json({
+            message: `Error: User not found with a user id of ${req.params.id}`
+        })
+    }
+
+    res.status(200).json({
+        user,
+    })
+}
+
+async function updateUsers(req,res,next) {
+    const newData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+        
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id, newData, { new: true })
+
+    res.status(200).json({
+        user,
+    });
+    
+}
+
+async function deleteUsers(req,res,next) {
+    const user = await User.findById(req.params.id);
+
+    if(!user) {
+        return res.status(404).json({
+            message: `Error: User not found with a user id of ${req.params.id}`
+        })
+    }
+
+    await user.deleteOne();
+
+    res.status(200).json({
+        message: `User with the user id of ${req.params.id} has been successfully deleted.`,
     })
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const { checkAuthentication } = require('../utility/checkAuth');
+const { checkAuthentication, authorizeRoles } = require('../utility/checkAuth');
 const router = express.Router();
 
 const userCtrl = require('../controllers/users')
 
 /* GET users listing. */
 router.get('/profile', checkAuthentication, userCtrl.getUsers)
+
+router.put('/profile/update', checkAuthentication, userCtrl.updateProfile)
+
+router.get('/admin/users', checkAuthentication, authorizeRoles('admin'), userCtrl.getAllUsers)
+
+router.get('/admin/users/:id', checkAuthentication, authorizeRoles('admin'), userCtrl.userDetails)
+
+router.put('/admin/users/:id', checkAuthentication, authorizeRoles('admin'), userCtrl.updateUsers)
+
+router.delete('/admin/users/:id', checkAuthentication, authorizeRoles('admin'), userCtrl.deleteUsers)
 
 module.exports = router;
